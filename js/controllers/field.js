06761383e3efa6861996d7c86d54f65d6746604f
jripault <tluapir@gmail.com>
2014-03-04 22:30:37 +0100

    still problem with subiview rendering

--- js/controllers/field.js
+++ js/controllers/field.js
@@ -1,5 +1,18 @@
 App.FieldController = Ember.Controller.extend({
     init: function() {
         this.set('generators', this.store.find('generator'));
-    }
+    },
+    generatorView: null,
+    selectedGenerator: null,
+
+    generatorChange: function() {
+        this.get('generatorView').popObject();
+        var generator = this.get('selectedGenerator');
+        if (generator) {
+            this.get('model').set('generator', generator);
+            this.get('generatorView').pushObject(App.Views.NumberGenerator.create({
+                name: generator.get('name')
+            }))
+        }
+    }.observes('selectedGenerator')
 });
--- js/views/field.js
+++ js/views/field.js
@@ -1,19 +1,4 @@
 App.FieldView = Ember.View.extend({
-    templateName: 'views/field',
-    childViewsContainer: Ember.ContainerView.create({}),
-    selectedGenerator: null,
-
-    didInsertElement: function() {
-        this.childViewsContainer.appendTo(this.$('.view-container'));
-    },
-    generatorChange: function() {
-        this.childViewsContainer.popObject();
-        var generator = this.get('selectedGenerator');
-        if (generator) {
-            this.get('model').set('generator', generator);
-            this.childViewsContainer.pushObject(App.Views.NumberGenerator.create({
-                name: generator.get('name')
-            }))
-        }
-    }.observes('selectedGenerator')
+    templateName: 'views/field'
+    
 });
