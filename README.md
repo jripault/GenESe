d2e52e893279f5861effb04bfb8721632c87e3c8
jripault <tluapir@gmail.com>
2014-03-05 22:59:20 +0100

    finally made it work: display generator view

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+GenESe
+======
+
+My attempt to do an ElasticSearch plugin for data generation
+
+Lots of ideas for it but taking time to realize them.
+
+Based on [Ember.js](http://emberjs.com/)
+
+
+How to run
+----------
+
 Need npm and Grunt to compile Handlebars templates
 
 Install dependencies through npm
--- js/views/field.js
+++ js/views/field.js
@@ -1,4 +1,15 @@
 App.FieldView = Ember.View.extend({
-    templateName: 'views/field'
-    
+    templateName: 'views/field',
+    generatorView: null,
+    generatorChange: function() {
+        if (this.generatorView) {
+            this.generatorView.popObject();
+            var generator = this.get('controller.selectedGenerator');
+            if (generator) {
+                this.generatorView.pushObject(App.Views.NumberGenerator.create({
+                    name: generator.get('name')
+                }))
+            }
+        }
+    }.observes('controller.selectedGenerator')
 });
